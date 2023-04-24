a14f52cbf8e96b30cc86437f674b62b487836518
ddmkim94 <ddmkim94@gmail.com>
2023-04-24 14:33:27 +0900

    추가 세팅
    - Querydsl 설정 추가
    - 스프링 시큐리티 설정 추가
    - Jpa Auditing 추가
    - yml 파일 추가

--- src/main/java/com/ll/social/SocialApplication.java
+++ src/main/java/com/ll/social/SocialApplication.java
@@ -2,12 +2,12 @@ package com.ll.social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SocialApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
 	}
-
 }
