92fde1915883b80b171c7afb3c2193ca611de25c
ddmkim94 <ddmkim94@gmail.com>
2023-04-25 17:12:27 +0900

    모든 액션마다 작성해줬던 현재 로그인한 회원의 정보와 프로필 이미지를 가져오는 로직 제거
    컨트롤러에서 Principal 객체를 사용하는 방식에서 타임리프에서 #authentication을 사용하는 방식으로 변경
    컨트롤러에 있던 복잡한 로직들이 전부 사라짐

--- src/main/java/com/ll/social/app/home/controller/HomeController.java
+++ src/main/java/com/ll/social/app/home/controller/HomeController.java
@@ -16,25 +16,12 @@ public class HomeController {
     private final MemberService memberService;
 
     @GetMapping("/")
-    public String main(Principal principal, Model model) {
-        Member loginedMember = null;
-        String loginedMemberProfileImgUrl = null;
-
-        if (principal != null && principal.getName() != null) {
-            loginedMember = memberService.getMemberByUsername(principal.getName());
-        }
-
-        if (loginedMember != null) {
-            loginedMemberProfileImgUrl = loginedMember.getProfileImgUrl();
-        }
-
-        model.addAttribute("loginedMember", loginedMember);
-        model.addAttribute("loginedMemberProfileImgUrl", loginedMemberProfileImgUrl);
+    public String main() {
         return "home/main";
     }
 
     @GetMapping("/about")
-    public String about(Principal principal, Model model) {
+    public String about() {
         return "home/about";
     }
 }
