ba2ccd61ff05a16d7a9376cb142e1abde2c78717
ddmkim94 <ddmkim94@gmail.com>
2023-04-25 18:06:09 +0900

    Principal 객체를 통해 컨트롤러에서 현재 로그인 한 회원의 정보를 얻는 방법

--- src/main/java/com/ll/social/app/home/controller/HomeController.java
+++ src/main/java/com/ll/social/app/home/controller/HomeController.java
@@ -3,9 +3,11 @@ package com.ll.social.app.home.controller;
 import com.ll.social.app.member.entity.Member;
 import com.ll.social.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
 
@@ -23,5 +25,12 @@ public class HomeController {
     @GetMapping("/about")
     public String about() {
         return "home/about";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/currentUserOrigin")
+    @ResponseBody
+    public Principal currentUserOrigin(Principal principal) {
+        return principal;
     }
 }
