6164091b98ef78e3efe255c107e05f112de131ae
ddmkim94 <ddmkim94@gmail.com>
2023-04-25 11:15:11 +0900

    특정 회원으로 로그인 했을 때 프로필이 제대로 나오는지 테스트
    테스트 실행시에 초기 데이터를 생성하기 위해 TestInitData 클래스 추가

--- src/main/java/com/ll/social/app/member/controller/MemberController.java
+++ src/main/java/com/ll/social/app/member/controller/MemberController.java
@@ -7,12 +7,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @Controller
--- src/test/java/com/ll/social/SocialApplicationTests.java
+++ src/test/java/com/ll/social/SocialApplicationTests.java
@@ -1,13 +1,13 @@
 package com.ll.social;
 
 import com.ll.social.app.home.controller.HomeController;
+import com.ll.social.app.member.controller.MemberController;
 import com.ll.social.app.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -51,9 +52,40 @@ class SocialApplicationTests {
 
 	@Test
 	@DisplayName("회원의 수")
-	@Rollback(value = false)
 	void t2() throws Exception {
 		long count = memberService.count();
 		assertThat(count).isGreaterThanOrEqualTo(5L);
+	}
+
+	@Test
+	@DisplayName("user1로 로그인 후 프로필 페이지에 접속하면 user1의 이메일이 보여야한다.")
+	void t3() throws Exception {
+
+		ResultActions resultActions = mvc.perform(get("/member/profile")
+						.with(user("user1").password("1234").roles("user"))
+				)
+				.andDo(print());
+
+		resultActions
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(MemberController.class))
+				.andExpect(handler().methodName("profile"))
+				.andExpect(content().string(containsString("user1@naver.com")));
+	}
+
+	@Test
+	@DisplayName("user4로 로그인 후 프로필 페이지에 접속하면 user4의 이메일이 보여야한다.")
+	void t4() throws Exception {
+		ResultActions resultActions = mvc.perform(get("/member/profile")
+						.with(user("user4").password("1234").roles("user"))
+				)
+				.andDo(print());
+
+		resultActions
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(MemberController.class))
+				.andExpect(handler().methodName("profile"))
+				.andExpect(content().string(containsString("user4@naver.com")));
+
 	}
 }
