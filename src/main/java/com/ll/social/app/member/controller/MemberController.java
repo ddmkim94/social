612df0223520b3f8fcf08a037d46f3df6a0e2cb0
ddmkim94 <ddmkim94@gmail.com>
2023-04-25 19:21:24 +0900

    프로필이미지 URL 줄이기
    리다이렉트를 사용해서 줄임.
    select 쿼리를 한 번 호출하는 단점이 있지만, 깔끔한 Url을 사용하고 싶은 경우 도입
    추후에 캐시 도입 예정

--- src/main/java/com/ll/social/app/member/controller/MemberController.java
+++ src/main/java/com/ll/social/app/member/controller/MemberController.java
@@ -9,10 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
@@ -77,5 +74,10 @@ public class MemberController {
     @GetMapping("/profile")
     public String profile() {
         return "member/profile";
+    }
+
+    @GetMapping("/profile/img/{id}")
+    public String profileImg(@PathVariable Long id) {
+        return "redirect:" + memberService.getMemberById(id).getProfileImgUrl();
     }
 }
