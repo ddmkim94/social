0da952d10304f36be1db53fa7a45abc2eae1a544
ddmkim94 <ddmkim94@gmail.com>
2023-04-27 14:54:59 +0900

    테스트 코드 수정
    Mock 유저로는 MemberContext가 실행되지않는다.
    @WithUserDetails를 사용해서 로그인 후 테스트가 진행되도록 수정함.

--- src/test/java/com/ll/social/SocialApplicationTests.java
+++ src/test/java/com/ll/social/SocialApplicationTests.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -69,12 +71,10 @@ class SocialApplicationTests {
 
 	@Test
 	@DisplayName("user1로 로그인 후 프로필 페이지에 접속하면 user1의 이메일이 보여야한다.")
+	@WithUserDetails("user1")
 	void t3() throws Exception {
 
-		ResultActions resultActions = mvc.perform(get("/member/profile")
-						.with(user("user1").password("1234").roles("user"))
-				)
-				.andDo(print());
+		ResultActions resultActions = mvc.perform(get("/member/profile")).andDo(print());
 
 		resultActions
 				.andExpect(status().is2xxSuccessful())
@@ -85,11 +85,9 @@ class SocialApplicationTests {
 
 	@Test
 	@DisplayName("user4로 로그인 후 프로필 페이지에 접속하면 user4의 이메일이 보여야한다.")
+	@WithUserDetails("user4")
 	void t4() throws Exception {
-		ResultActions resultActions = mvc.perform(get("/member/profile")
-						.with(user("user4").password("1234").roles("user"))
-				)
-				.andDo(print());
+		ResultActions resultActions = mvc.perform(get("/member/profile")).andDo(print());
 
 		resultActions
 				.andExpect(status().is2xxSuccessful())
