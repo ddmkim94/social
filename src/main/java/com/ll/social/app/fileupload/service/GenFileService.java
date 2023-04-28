f2a6b29f16f42e46b225b4d00a6e15e9f068932a
ddmkim94 <ddmkim94@gmail.com>
2023-04-28 13:49:32 +0900

    글쓰기 시 파일의 메타 정보를 가져와서 저장
    메타정보는 파일에 대한 추가 정보를 말함.

--- src/main/java/com/ll/social/app/fileupload/service/GenFileService.java
+++ src/main/java/com/ll/social/app/fileupload/service/GenFileService.java
@@ -3,6 +3,7 @@ package com.ll.social.app.fileupload.service;
 import com.ll.social.app.article.entity.Article;
 import com.ll.social.app.fileupload.entity.GenFile;
 import com.ll.social.app.fileupload.repository.GenFileRepository;
+import com.ll.social.app.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,15 +24,18 @@ public class GenFileService {
         for (String key : fileMap.keySet()) {
             MultipartFile multipartFile = fileMap.get(key);
 
-            String typeCode = "common";
-            String type2Code = "inBodyImg";
-            String fileExt = "jpg";
-            String fileExtTypeCode = "img";
-            String fileExtType2Code = "jpg";
-            int fileNo = 1;
-            int fileSize = 1000;
-            String fileDir = "article/2023_04_28";
-            String originFileName = "??";
+            String[] inputNameBits = key.split("__");
+
+            String originFileName = multipartFile.getOriginalFilename();
+            String ext = Util.file.getExt(originFileName);
+            String typeCode = inputNameBits[0];
+            String type2Code = inputNameBits[1];
+            String fileExt = ext;
+            String fileExtTypeCode = Util.file.getFileExtTypeCodeFromFileExt(fileExt);
+            String fileExtType2Code = Util.file.getFileExtType2CodeFromFileExt(fileExt);
+            int fileNo = Integer.parseInt(inputNameBits[2]);
+            int fileSize = (int) multipartFile.getSize();
+            String fileDir = relTypeCode  +"/" + Util.date.getCurrentDateFormatted("yyyy_MM_dd");
 
             GenFile genFile = GenFile.builder()
                     .relTypeCode(relTypeCode)
