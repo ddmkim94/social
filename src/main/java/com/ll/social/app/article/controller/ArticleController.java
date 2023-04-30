cce4ae0dec270580f079404a1df8fd422aefed81
ddmkim94 <ddmkim94@gmail.com>
2023-04-30 15:46:43 +0900

    게시물 수정 폼 구현

--- src/main/java/com/ll/social/app/article/controller/ArticleController.java
+++ src/main/java/com/ll/social/app/article/controller/ArticleController.java
@@ -10,6 +10,7 @@ import com.ll.social.app.security.dto.MemberContext;
 import com.ll.social.util.Util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartRequest;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -67,5 +69,28 @@ public class ArticleController {
         model.addAttribute("article", article);
 
         return "article/detail";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/{id}/modify")
+    public String modifyForm(@AuthenticationPrincipal MemberContext memberContext, @PathVariable Long id, Model model) {
+        Article article = articleService.getForPrintArticleById(id);
+
+        if (memberContext.getId() != article.getMember().getId()) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN); // 403
+        }
+
+        model.addAttribute("article", article);
+
+        return "article/modify";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/{id}/modify")
+    public String modify(@PathVariable Long id, @Valid ArticleForm articleForm) {
+
+        Article article = articleService.getForPrintArticleById(id);
+
+        return "redirect:/article/%d".formatted(id);
     }
 }
