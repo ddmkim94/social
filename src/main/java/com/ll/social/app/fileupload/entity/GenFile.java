d7ae4cecf9fd258a4e460834f2cbe6ade59a9cfc
ddmkim94 <ddmkim94@gmail.com>
2023-05-01 14:51:07 +0900

    게시물 수정 폼에서 기존 이미지 업데이트 기능 구현

--- src/main/java/com/ll/social/app/fileupload/entity/GenFile.java
+++ src/main/java/com/ll/social/app/fileupload/entity/GenFile.java
@@ -1,6 +1,7 @@
 package com.ll.social.app.fileupload.entity;
 
 
+import com.ll.social.app.base.AppConfig;
 import com.ll.social.app.base.entity.BaseEntity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -37,11 +38,29 @@ public class GenFile extends BaseEntity {
 
     private String originFileName;
 
+    public void merge(GenFile other) {
+        relTypeCode = other.getRelTypeCode();
+        relId = other.getRelId();
+        typeCode = other.getTypeCode();
+        type2Code = other.getType2Code();
+        fileExtTypeCode = other.getFileExtTypeCode();
+        fileExtType2Code = other.getFileExtType2Code();
+        fileSize = other.getFileSize();
+        fileNo = other.getFileNo();
+        fileExt = other.getFileExt();
+        fileDir = other.getFileDir();
+        originFileName = other.getOriginFileName();
+    }
+
     public String getFileName() {
         return getId() + "." + getFileExt();
     }
 
     public String getUrl() {
         return "/gen/" + getFileDir() + "/" + getFileName();
+    }
+
+    public String getFilePath() {
+        return AppConfig.FILE_DIR_PATH + "/" + getFileDir() + "/" + getFileName();
     }
 }
--- src/main/java/com/ll/social/app/fileupload/service/GenFileService.java
+++ src/main/java/com/ll/social/app/fileupload/service/GenFileService.java
@@ -8,6 +8,7 @@ import com.ll.social.app.fileupload.repository.GenFileRepository;
 import com.ll.social.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -67,7 +68,7 @@ public class GenFileService {
                     .originFileName(originFileName)
                     .build();
 
-            genFileRepository.save(genFile);
+            genFile = save(genFile);
 
             // 파일 업로드 (저장)
             String filePath = AppConfig.FILE_DIR_PATH + fileDir + "/" + genFile.getFileName();
@@ -84,6 +85,30 @@ public class GenFileService {
         }
 
         return new RsData<>("S-1", "파일을 업로드했습니다.", genFileIds);
+    }
+
+    @Transactional
+    public GenFile save(GenFile genFile) {
+        Optional<GenFile> opOldGenFile = genFileRepository.findByRelTypeCodeAndRelIdAndTypeCodeAndType2CodeAndFileNo(
+                genFile.getRelTypeCode(), genFile.getRelId(), genFile.getTypeCode(), genFile.getType2Code(), genFile.getFileNo()
+        );
+
+        if (opOldGenFile.isPresent()) { // 파일이 존재한다면
+            GenFile oldGenFile = opOldGenFile.get();
+            deleteFileFromStorage(oldGenFile);
+
+            oldGenFile.merge(genFile);
+
+            genFileRepository.save(oldGenFile);
+
+            return oldGenFile;
+        }
+
+        return genFileRepository.save(genFile);
+    }
+
+    private void deleteFileFromStorage(GenFile genFile) {
+        new File(genFile.getFilePath()).delete();
     }
 
     public void addGenFileByUrl(String relTypeCode, Long relId, String typeCode, String type2Code, int fileNo, String url) {
